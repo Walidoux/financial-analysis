7b74cdbc58a87cd6c1f9ddafb0b79c5e8b31be1a
Walidoux <ma.walidkorchi@icloud.com>
2026-01-04 22:56:35 +0100

    feat(config): add vite plugin html minifier

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import { resolve } from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import htmlMinifier from 'vite-plugin-html-minifier'
 import solidPlugin from 'vite-plugin-solid'
 import pkg from './package.json'
 
 export default defineConfig({
-  plugins: [solidPlugin(), tailwindcss()],
+  plugins: [solidPlugin(), tailwindcss(), htmlMinifier({ minify: true })],
   base: `/${pkg.name}/`,
   server: {
     port: 3000,
